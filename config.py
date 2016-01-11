0cbbde35a9db811a628bb78882ce9aaa94afe745
Nick Marlin <nick.xmar@gmail.com>
2016-01-11 14:45:09 -0700

    Add database location to configurate values.

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
 # This file contains most of the configuration variables
 DEBUG=True
+
+DATABASE='/home/nick/projects/improv_registration/guests.db'
+SECRET_KEY='development_key'
+USERNAME = 'admin'
+PASSWORD = 'default'
