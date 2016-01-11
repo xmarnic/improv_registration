6177d476bdc52cddcc4162cdf01043631c4d2777
Nick Marlin <nick.xmar@gmail.com>
2016-01-10 21:00:13 -0700

    Create register() and app route. Currently, working on handling a POST request.

--- reg_app.py
+++ reg_app.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
@@ -7,6 +7,14 @@ app.config.from_pyfile('config.py')
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('register.html')
 
 
 # Fire up server if file ran as standalone application.
