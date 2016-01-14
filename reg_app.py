e4009cc2f2a356740ac7d74a70e94e8dc4fe8e77
Nick Marlin <nick.xmar@gmail.com>
2016-01-14 16:25:30 -0700

    Use PostgreSQL and psycopg2.

--- reg_app.py
+++ reg_app.py
@@ -1,24 +1,14 @@
 from __future__ import print_function
 from flask import Flask, flash, g, render_template, request, url_for
-from contextlib import closing
-import sqlite3
-
+import psycopg2
 
 # Application SetUP\p
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
-
 # DATABASE
 def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
-
-# Create database with Python as opposed to sqlite3
-def init_db():
-    with closing(connect_db()) as db:
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
+    return psycopg2.connect(database="improv_reg", user="nick", password="zeP7aicoopa", host="127.0.0.1")
 
 # Connect to db before each request
 @app.before_request
@@ -38,8 +28,10 @@ def teardown_request(exception):
 def register():
     error = None
     if request.method == 'POST':
-        g.db.execute('insert into guests (first_name, last_name, email_address) values (?, ?, ?)',
-                     [request.form['first_name'], request.form['last_name'], request.form['email']])
+        first_name, last_name, email_address = request.form.values()
+        sql_statement = "INSERT INTO guests (guest_id, first_name, last_name, email_address) VALUES (DEFAULT, %s, %s, %s)"
+        cur = g.db.cursor()
+        cur.execute(sql_statement, (first_name, last_name, email_address))
         g.db.commit()
         flash('You have successfully registered.')
     return render_template('registration_form.html', error=error)
@@ -47,8 +39,10 @@ def register():
 
 @app.route('/guests')
 def show_guests():
-    cur = g.db.execute('select first_name, last_name, email_address from guests order by id desc')
-    guests = [dict(first_name=row[0], last_name=row[1], email_address=row[2]) for row in cur.fetchall()]
+    cur = g.db.cursor()
+    cur.execute("SELECT * FROM guests;")
+    guests = [dict(first_name=row[1], last_name=row[2], email_address=row[3]) for row in cur.fetchall()]
+    cur.close()
     return render_template('show_guests.html', guests=guests)
 
 
