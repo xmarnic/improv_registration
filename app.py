6942d77aa47902ce742c4c7a82502c8947173fd3
Nick Marlin <nick.xmar@gmail.com>
2016-01-21 18:22:56 -0700

    Modify register() and show_guests() to read from Boolean field in guests table.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 from flask import Flask, flash, g, redirect, render_template, request, url_for
 from flask_wtf import Form
-from wtforms import StringField
+from wtforms import StringField, BooleanField
 from wtforms.validators import Required, Email
 import os
 import psycopg2
@@ -43,16 +43,16 @@ class RegForm(Form):
     first_name = StringField('First Name', validators=[Required()])
     last_name = StringField('Last Name', validators=[Required()])
     email = StringField('Email Address', validators=[Required(), Email()])
-
+    public = BooleanField('Public')
 
 # VIEWS
 @app.route('/', methods=['GET', 'POST'])
 def register():
     form = RegForm()
     if form.validate_on_submit():
-        sql_statement = "INSERT INTO guests (first_name, last_name, email_address) VALUES (%s, %s, %s)"
+        sql_statement = "INSERT INTO guests (first_name, last_name, email_address, public) VALUES (%s, %s, %s, %s)"
         cur = g.db.cursor()
-        cur.execute(sql_statement, (form.first_name.data, form.last_name.data, form.email.data))
+        cur.execute(sql_statement, (form.first_name.data, form.last_name.data, form.email.data, form.public.data))
         g.db.commit()
         return redirect(url_for('show_guests'))
     return render_template('reg_form.html', form=form)
@@ -60,7 +60,7 @@ def register():
 @app.route('/guests')
 def show_guests():
     cur = g.db.cursor()
-    cur.execute("SELECT * FROM guests")
+    cur.execute("SELECT * FROM guests WHERE public")
     guests = [dict(first_name=row[0], last_name=row[1], email_address=row[2]) for row in cur.fetchall()]
     cur.close()
     return render_template('show_guests.html', guests=guests)
