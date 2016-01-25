0bb321f629b7e1a3b72f01cece1869d2d5209351
Nick Marlin <nick.xmar@gmail.com>
2016-01-25 01:11:13 -0700

    Update SQL to insert city field.

--- app.py
+++ app.py
@@ -43,31 +43,38 @@ class RegForm(Form):
     first_name = StringField('First Name', validators=[Required()])
     last_name = StringField('Last Name', validators=[Required()])
     email = StringField('Email Address', validators=[Required(), Email()])
+    city = StringField('City', validators=[Required()])
     public = BooleanField('Public')
 
 
 # VIEWS
 @app.route('/', methods=['GET', 'POST'])
 def register():
+    alert_class = None
     form = RegForm()
     if form.validate_on_submit():
-        sql_statement = "INSERT INTO guests (first_name, last_name, email_address, public) VALUES (%s, %s, %s, %s)"
+        sql_statement = "INSERT INTO guests (first_name, last_name, email_address, city, public) VALUES (%s, %s, %s, %s, %s)"
         cur = g.db.cursor()
         cur.execute(sql_statement, (form.first_name.data.capitalize(),
             form.last_name.data.capitalize(), form.email.data.lower(),
-            form.public.data)
+            form.city.data.capitalize(), form.public.data)
         )
         g.db.commit()
+        alert_class = 'alert-success'
+        flash('You have successfully registered.')
         return redirect(url_for('show_guests'))
-    return render_template('reg_form.html', form=form, subtitle="Event Registration")
+    elif request.method == 'POST':
+        alert_class='alert-danger'
+        flash('Please complete the registration form.')
+    return render_template('reg_form.html', form=form, alert_class=alert_class, hidden=None)
 
 @app.route('/guestlist')
 def show_guests():
     cur = g.db.cursor()
-    cur.execute("SELECT first_name, last_name FROM guests WHERE public")
-    guests = [dict(first_name=row[0], last_name=row[1]) for row in cur.fetchall()]
+    cur.execute("SELECT first_name, last_name, city FROM guests WHERE public")
+    guests = [dict(first_name=row[0], last_name=row[1], city=row[2]) for row in cur.fetchall()]
     cur.close()
-    return render_template('guestlist.html', guests=guests, subtitle="Guest List")
+    return render_template('guestlist.html', guests=guests, alert_class='alert-success', hidden='hidden')
 
 
 # Fire up server if file ran as standalone application.
