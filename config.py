213220c8297fe9da261e0c642af7c4f728a9b6dc
Nick Marlin <nick.xmar@gmail.com>
2016-01-28 10:11:25 -0700

    Get debug from environ.

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # This file contains most of the configuration variables
 import os
 
-DEBUG=True
+DEBUG=os.environ.get('DEBUG')
 SECRET_KEY =os.environ.get('SECRET_KEY')
 DATABASE_URL=os.environ.get('DATABASE_URL')
